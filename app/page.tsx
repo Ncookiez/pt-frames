86a388f2317875de934666910bb1b661e48033b2
Ncookiez <ncukiert@gmail.com>
2024-03-23 11:13:44 -0400

    styled home page + added favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main>
-      <span>In construction...</span>
+    <main className='mt-8 flex h-full w-full flex-col items-center justify-center gap-8'>
+      <Image
+        src='/pt-logo.svg'
+        alt='PoolTogether'
+        width={716}
+        height={284}
+        priority={true}
+        className='h-20'
+      />
+      <h1 className='text-2xl font-bold'>Frames Available:</h1>
+      <ul>
+        <li>
+          <Link href='/prizeVault'>/prizeVault</Link>
+        </li>
+      </ul>
     </main>
   )
 }
